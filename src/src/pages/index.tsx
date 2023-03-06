a3a4c0a7276d619492601d163620e86675d90ceb
nawawa <oonawa12@gmail.com>
2023-03-06 10:30:32 +0900

    test: API Routes が Vercel でどう動くか動作確認

--- src/src/pages/index.tsx
+++ src/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Blog } from "../../types/blog"
 
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next"
 
-const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs }) => {
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs, hello }) => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <ul>
@@ -13,17 +13,20 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs 
           </li>
         ))}
       </ul>
+      { hello.name }
     </div>
   );
 }
 
-export const getStaticProps: GetStaticProps<{ blogs: Blog[] }> = async () => {
+export const getStaticProps: GetStaticProps<{ blogs: Blog[], hello: { name: string } }> = async () => {
   const data = await client.get({ endpoint: "blogs" });
   const blogs: Blog[] = data.contents
+  const hello: {name: string} = await fetch(`${process.env.SERVER_ORIGIN}/api/hello`).then(res => res.json())
 
   return {
     props: {
       blogs,
+      hello
     },
   };
 };
