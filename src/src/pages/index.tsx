04d8f0199ad6c9454a668c557c538cf3381a91e0
nawawa <oonawa12@gmail.com>
2023-03-06 13:26:45 +0900

    test:

--- src/src/pages/index.tsx
+++ src/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Blog } from "../../types/blog"
 
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next"
 
-const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs }) => {
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs, origin }) => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <ul>
@@ -13,17 +13,21 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs 
           </li>
         ))}
       </ul>
+      <p>{origin}</p>
     </div>
   );
 }
 
-export const getStaticProps: GetStaticProps<{ blogs: Blog[] }> = async () => {
+export const getStaticProps: GetStaticProps<{ blogs: Blog[], origin?: string }> = async () => {
   const data = await client.get({ endpoint: "blogs" });
   const blogs: Blog[] = data.contents
+
+  const origin = process.env.SERVER_ORIGIN
 
   return {
     props: {
       blogs,
+      origin
     },
   };
 };
